1c0630b2849178c30ce20e3933259dffae77a04c
vlas-dev <83783001+vlas-dev@users.noreply.github.com>
2023-11-28 11:54:54 -0300

    signin page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/shared/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import Footer from "./components/shared/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        
+        <Route path="/signin" element={<Signin />} />
         
       </Routes>
       </div>
