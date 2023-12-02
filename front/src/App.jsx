8c54dea6d1bf7ceb7ff6675223ceb2f6c93bd3ee
vlas-dev <83783001+vlas-dev@users.noreply.github.com>
2023-12-01 23:13:05 -0300

    user login & register

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Navbar from "./components/shared/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Footer from "./components/shared/Footer";
+import { CRMContext, CRMProvider } from "./components/context/CRMcontext";
 
 function AnimatedRoutes() {
   const location = useLocation();
 
+  // Animation variants
+  const pageTransition = {
+    in: {
+      opacity: 1,
+    },
+    out: {
+      opacity: 0,
+    },
+  };
+
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.key}
-        classNames="fade"
-        timeout={300}
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.2 }}
       >
         <Routes location={location}>
           <Route path="/" element={<Home />} />
@@ -24,19 +43,33 @@ function AnimatedRoutes() {
           <Route path="/signin" element={<Signin />} />
           {/* Add more routes as needed */}
         </Routes>
-      </CSSTransition>
-    </TransitionGroup>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
 export default function App() {
+  const [auth, setAuth] = useState({
+    token: "",
+    isAuthenticated: false,
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem("x-token");
+    if (token) {
+      setAuth({ token, isAuthenticated: true });
+    }
+  }, []);
+
   return (
-    <Router>
-      <Navbar />
-      <div className="min-h-screen bg-gray-100">
-        <AnimatedRoutes />
-      </div>
-      <Footer />
-    </Router>
+    <CRMProvider value={[auth, setAuth]}>
+      <Router>
+        <Navbar />
+        <div className="min-h-screen bg-gray-100">
+          <AnimatedRoutes />
+        </div>
+        <Footer />
+      </Router>
+    </CRMProvider>
   );
 }
--- front/src/components/shared/Navbar.jsx
+++ front/src/components/shared/Navbar.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { RiSearchLine, RiShoppingCartLine } from "react-icons/ri";
+import { IoIosArrowDown } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa6";
+
 import { Twirl as Hamburger } from "hamburger-react";
 import logoImage from "/assets/logo.png";
-
+import { CRMContext } from "../../components/context/CRMcontext";
 export default function Navbar() {
+  const [auth, setAuth] = useContext(CRMContext);
+  console.log(auth);
+
   const [isOpen, setOpen] = useState(false); // State to control the hamburger menu visibility
-  const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+  const [showSubmenu, setShowSubmenu] = useState(false);
+  const [showProfileSubmenu, setShowProfileSubmenu] = useState(false);
+
+  const navigate = useNavigate();
+
   const categories = [
     {
       id: 1,
@@ -25,10 +35,24 @@ export default function Navbar() {
     },
   ];
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem("x-token");
+    if (storedToken) {
+      setAuth({ token: storedToken, isAuthenticated: true });
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setAuth({ token: null, isAuthenticated: false });
+    localStorage.removeItem("x-token");
+    setOpen(false);
+    navigate("/");
+  };
+
   return (
     <div className="bg-gray-800 fixed z-50 w-full">
-      <nav className="p-2 flex flex-col items-center md:mx-10 lg:mx-56">
-        <div className="w-full flex justify-between items-center">
+      <nav className="p-2 items-center md:mx-10 lg:mx-40">
+        <div className="max-w-[1200px] mx-auto flex justify-between items-center">
           <Link to="/" className="text-2xl font-bold w-20 mr-8 md:mr-16">
             <img src={logoImage} alt="Logo" />
           </Link>
@@ -51,28 +75,70 @@ export default function Navbar() {
 
           <div className="hidden md:flex space-x-4 font-semibold text-white">
             <div className="relative group items-center">
-              <button className="p-2">Categorías</button>
-              <div className="dropdown-content hidden absolute bg-white shadow-lg group-hover:block">
-                {categories.map((category) => (
-                  <Link key={category.id} to={category.link}>
-                    <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
-                      {category.name}
-                    </button>
-                  </Link>
-                ))}
+              <button className="p-2 flex items-center justify-center gap-2">
+                Categorías <IoIosArrowDown />
+              </button>
+              <div className="absolute p-2 opacity-0 group-hover:opacity-100 group-hover:scale-100 scale-95 bg-white rounded text-gray-800 transform transition-all duration-300 ease-in-out flex flex-col font-bold pointer-events-none group-hover:pointer-events-auto">
+                {/* The submenu div now remains visible when hovering over the links */}
+                <a
+                  onClick={() => setOpen(false)}
+                  href="/celulares"
+                  className="p-2 hover:bg-gray-200 rounded"
+                >
+                  Celulares
+                </a>
+                <a
+                  onClick={() => setOpen(false)}
+                  href="/notebooks"
+                  className="p-2 hover:bg-gray-200 rounded"
+                >
+                  Notebooks
+                </a>
+                <a
+                  onClick={() => setOpen(false)}
+                  href="/tablets"
+                  className="p-2 hover:bg-gray-200 rounded"
+                >
+                  Tablets
+                </a>
               </div>
             </div>
 
-            <Link to="/signin">
-              <button className="p-2">
-                Ingresar
-              </button>
-            </Link>
-            <Link to="/signup">
-              <button className="bg-blue-600 hover:bg-blue-800 p-2 rounded font-bold">
-                Registrarse
-              </button>
-            </Link>
+            {auth.isAuthenticated ? (
+              // Logged in user view
+              <>
+                <div className="relative group items-center">
+                  <button className="p-3 flex items-center justify-center gap-2">
+                    <FaRegUser /> <IoIosArrowDown />
+                  </button>
+                  <div className="absolute p-2 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 group-hover:scale-100 scale-95 bg-white rounded text-gray-800 transition-all duration-300 ease-in-out flex flex-col font-bold pointer-events-none group-hover:pointer-events-auto">
+                    {/* The submenu div now remains visible when hovering over the button group */}
+                    <Link
+                      to="/dashboard"
+                      className="p-2 hover:bg-gray-200 rounded text-start"
+                    >
+                      Dashboard
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="p-2 hover:bg-gray-200 rounded text-start"
+                    >
+                      Salir
+                    </button>
+                  </div>
+                </div>
+              </>
+            ) : (
+              // Guest user view
+              <>
+                <Link className="p-2" to="/signin">
+                  Ingresar
+                </Link>
+                <Link className="p-2" to="/signup">
+                  Registrarse
+                </Link>
+              </>
+            )}
             <button className="p-2">
               <RiShoppingCartLine size={24} aria-label="Shopping Cart" />
             </button>
@@ -94,39 +160,100 @@ export default function Navbar() {
 
         {/* Hamburger Menu Content */}
         <div
-          className={`fixed h-screen w-full bg-black bg-opacity-50 transition-opacity ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-            }`}
+          className={`fixed h-screen w-full bg-black bg-opacity-50 transition-opacity ${
+            isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
           onClick={() => setOpen(false)}
         ></div>
         <ul
-          className={`md:hidden fixed top-0 right-0 h-screen w-3/5 px-10 space-y-8 bg-gray-800 flex flex-col pt-32 text-white text-xl transform ${isOpen ? "translate-x-0" : "translate-x-full"
-            } transition duration-300 ease-in-out`}
+          className={`md:hidden fixed top-0 right-0 h-screen w-3/5 px-10 space-y-8 bg-gray-800 flex flex-col pt-32 text-white text-xl transform ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          } transition duration-300 ease-in-out`}
         >
-          <Link to="/signin">
-            <button onClick={() => setOpen(!isOpen)} className="p-2">
-              Ingresar
-            </button>
-          </Link>
-          <Link to="/signup">
-            <button onClick={() => setOpen(!isOpen)} className="bg-blue-600 hover:bg-blue-800 p-2 rounded font-bold">
-              Registrarse
-            </button>
-          </Link>
+          {auth.isAuthenticated ? (
+            // Logged in user view
+            <>
+              <div className="relative group items-center">
+                <button
+                  className="p-2 flex items-center justify-center gap-2"
+                  onClick={() => setShowProfileSubmenu(!showProfileSubmenu)}
+                >
+                  Mi Cuenta <IoIosArrowDown />
+                </button>
+                <div
+                  className={`${
+                    showProfileSubmenu ? "block" : "hidden"
+                  } flex flex-col font-bold`}
+                >
+                  <a
+                    onClick={() => setOpen(false)}
+                    href="/celulares"
+                    className="p-2 my-2"
+                  >
+                    Dashboard
+                  </a>
+                  <a
+                    onClick={() => {
+                      handleLogout();
+                      setShowProfileSubmenu(!showProfileSubmenu);
+                    }}
+                    className="p-2"
+                  >
+                    Salir
+                  </a>
+                </div>
+              </div>
+            </>
+          ) : (
+            // Guest user view
+            <>
+              <Link
+                onClick={() => setOpen(!isOpen)}
+                className="p-2"
+                to="/signin"
+              >
+                Ingresar
+              </Link>
+              <Link
+                onClick={() => setOpen(!isOpen)}
+                className="p-2"
+                to="/signup"
+              >
+                Registrarse
+              </Link>
+            </>
+          )}
           <div className={`relative group items-center`}>
-            <button className="p-2" onClick={() => setSubmenuOpen(!isSubmenuOpen)}>
-              Categorías
+            <button
+              className="p-2 flex items-center justify-center gap-2"
+              onClick={() => setShowSubmenu(!showSubmenu)}
+            >
+              Categorías <IoIosArrowDown />
             </button>
-            <div className={`dropdown-content ${isSubmenuOpen ? "block" : "hidden"} absolute bg-white shadow-lg z-50`}>
-              {categories.map((category) => (
-                <Link key={category.id} to={category.link}>
-                  <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
-                    {category.name}
-                  </button>
-                </Link>
-              ))}
+            <div
+              className={`${
+                showSubmenu ? "block" : "hidden"
+              } flex flex-col font-bold`}
+            >
+              <a
+                onClick={() => setOpen(false)}
+                href="/celulares"
+                className="p-2 my-2"
+              >
+                Celulares
+              </a>
+              <a
+                onClick={() => setOpen(false)}
+                href="/notebooks"
+                className="p-2 mb-2"
+              >
+                Notebooks
+              </a>
+              <a onClick={() => setOpen(false)} href="/tablets" className="p-2">
+                Tablets
+              </a>
             </div>
           </div>
-
         </ul>
       </nav>
     </div>
