68f8d354e0d33bdda39a08143d9fe09eece9a1e6
vlas237 <83783001+vlas237@users.noreply.github.com>
2023-12-19 11:52:09 -0300

    editmispublicaciones component

--- front/src/components/me/MisPublicaciones.jsx
+++ front/src/components/me/MisPublicaciones.jsx
@@ -3,24 +3,15 @@ import { Link } from "react-router-dom";
 import crudAxios from "../../config/axios";
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin7Line, RiDeleteBin7Fill } from "react-icons/ri";
+import EditMisPublicaciones from "./EditMisPublicaciones";
 
 export default function MisPublicaciones() {
   const [hoveredItemId, setHoveredItemId] = useState(null);
-
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
-  const [isNew, setIsNew] = useState(false); // State to track if adding a new product
-  const [editProduct, setEditProduct] = useState({
-    id: "", // Added id to track which product is being edited or added
-    titulo: "",
-    precio: "",
-    category: "",
-    marca: "",
-    envio: "",
-    descripcion: "",
-    imagen: "",
-  });
+  const [isNew, setIsNew] = useState(false);
+  const [editProduct, setEditProduct] = useState({});
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -37,43 +28,33 @@ export default function MisPublicaciones() {
     fetchProducts();
   }, []);
 
-  const handleInputChange = (e) => {
-    setEditProduct({ ...editProduct, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (isNew) {
-        // Add new product request
-        await crudAxios.post("/product/add", editProduct);
-      } else if (isEditing) {
-        // Update product request
-        await crudAxios.put("/product/update", editProduct);
-      }
-      // Refresh products list after update or add
-      const res = await crudAxios.get("/product");
-      setProducts(res.data);
-      setIsEditing(false);
-      setIsNew(false);
-    } catch (error) {
-      console.error("Error updating/adding product:", error);
-    }
-  };
-
   const handleAddNew = () => {
     setIsNew(true);
     setIsEditing(false);
     setEditProduct({
-      id: "",
       titulo: "",
       precio: "",
       category: "",
       marca: "",
-      envio: "",
+      envio: true,
+      descuento:0,
       descripcion: "",
       imagen: "",
     });
+  };
+
+  const handleEdit = (product) => {
+    setIsEditing(true);
+    setEditProduct(product);
+  };
+
+  const handleRemove = async (productId) => {
+    // Removal logic here
+  };
+
+  const refreshProducts = async () => {
+    const res = await crudAxios.get("/product");
+    setProducts(res.data);
   };
 
   if (isLoading) {
@@ -87,96 +68,15 @@ export default function MisPublicaciones() {
   return (
     <div className="container mx-auto p-4">
       {isNew || isEditing ? (
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[550px] md:w-[1000px] "
-        >
-          <div className="mb-4">
-            <h1>Producto</h1>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="titulo"
-              placeholder="Title"
-              value={editProduct.titulo}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="mb-4">
-            <h1>Precio</h1>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="precio"
-              placeholder="Price"
-              value={editProduct.precio}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="mb-4">
-            <h1>Marca</h1>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="marca"
-              placeholder="Brand"
-              value={editProduct.marca}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="mb-4">
-            <h1>Envío</h1>
-            <select
-              className=" bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="envio"
-              onChange={handleInputChange}
-              value={editProduct.envio ? "Envío Gratis" : "Sin Envío Incluido"}
-            >
-              <option value="Envío Gratis">Envío Gratis</option>
-              <option value="Sin Envío Incluido">Sin Envío Incluido</option>
-            </select>
-          </div>
-
-          <div className="mb-4">
-            <h1>Categoría</h1>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="category"
-              placeholder="Category"
-              value={editProduct.category}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="mb-4">
-            <h1>Descripción</h1>
-            <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="descripcion"
-              placeholder="Description"
-              value={editProduct.descripcion}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="flex flex-col items-center justify-between space-y-2">
-            <button
-              className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              {isNew ? "Add Product" : "Save Changes"}
-            </button>
-            <button
-              className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              onClick={() => {
-                setIsEditing(false);
-                setIsNew(false);
-              }}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
+        <EditMisPublicaciones
+          isNew={isNew}
+          isEditing={isEditing}
+          editProduct={editProduct}
+          setEditProduct={setEditProduct}
+          setIsEditing={setIsEditing}
+          setIsNew={setIsNew}
+          refreshProducts={refreshProducts}
+        />
       ) : (
         <div>
           <div className="flex justify-between items-center mb-4">
@@ -200,7 +100,9 @@ export default function MisPublicaciones() {
                     >
                       <img
                         className="w-20 h-20 object-cover rounded"
+                      //  LLAMAR IMAGENES DESDE EL BACK // src={`${import.meta.env.VITE_APP_BACKEND_URL}/uploads/productos/${product.imagen}`}
                         src={`/images/products/${product.titulo}.png`}
+
                         alt={product.titulo}
                       />
                       <div>
@@ -211,11 +113,7 @@ export default function MisPublicaciones() {
 
                     <div className="flex items-center space-x-2 text-indigo-600">
                       <button
-                        className=""
-                        onClick={() => {
-                          setIsEditing(true);
-                          setEditProduct(product);
-                        }}
+                        onClick={() => handleEdit(product)}
                       >
                         <FaRegEdit />
                       </button>
